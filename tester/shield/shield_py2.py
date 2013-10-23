fcd47002e85e505e507018c237ba72f91dafb293
mjnaderi <mjnaderi@gmail.com>
2013-10-23 16:26:09 +0330

    Minor Change

--- tester/shield/shield_py2.py
+++ tester/shield/shield_py2.py
@@ -1,23 +1,19 @@
 # @file shield_py2.py
 
-def shj_py2_shield():
-  BLACKLIST = [
-    #'__import__', # deny importing modules
-    'eval', # eval is evil
-    'open',
-    'file',
-    'exec',
-    'execfile',
-    'compile',
-    'reload',
-    'input' # input in python 2 uses eval
-    ]
-  for func in BLACKLIST:
-    if func in __builtins__.__dict__:
-      del __builtins__.__dict__[func]
-
 import sys
 sys.modules['os']=None
 
-# enabling shield:
-shj_py2_shield()
+BLACKLIST = [
+  #'__import__', # deny importing modules
+  'eval', # eval is evil
+  'open',
+  'file',
+  'exec',
+  'execfile',
+  'compile',
+  'reload',
+  'input' # input in python 2 uses eval
+  ]
+for func in BLACKLIST:
+  if func in __builtins__.__dict__:
+    del __builtins__.__dict__[func]
--- tester/shield/shield_py3.py
+++ tester/shield/shield_py3.py
@@ -1,23 +1,19 @@
 # @file shield_py3.py
 
-def shj_py3_shield():
-  BLACKLIST = [
-    #'__import__', # deny importing modules
-    'eval', # eval is evil
-    'open',
-    'file',
-    'exec',
-    'execfile',
-    'compile',
-    'reload',
-    #'input'
-    ]
-  for func in BLACKLIST:
-    if func in __builtins__.__dict__:
-      del __builtins__.__dict__[func]
-
 import sys
 sys.modules['os']=None
 
-# enabling shield:
-shj_py3_shield()
+BLACKLIST = [
+  #'__import__', # deny importing modules
+  'eval', # eval is evil
+  'open',
+  'file',
+  'exec',
+  'execfile',
+  'compile',
+  'reload',
+  #'input'
+  ]
+for func in BLACKLIST:
+  if func in __builtins__.__dict__:
+    del __builtins__.__dict__[func]
