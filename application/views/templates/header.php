94b5a4fc0228cd976a4830fc2ee66e990446c61c
mjnaderi <mjnaderi@gmail.com>
2013-11-03 16:31:15 +0330

    Use <!DOCTYPE html>

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,7 +6,7 @@
  */
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
 <html>
 <head>
 	<title><?php echo $title ?> - Sharif Judge</title>
