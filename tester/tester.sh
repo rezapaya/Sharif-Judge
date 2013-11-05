434566a3c7e489ed4063c07f225f6d0a255770ac
mjnaderi <mjnaderi@gmail.com>
2013-11-05 20:55:47 +0330

    Add a newline at the end of output files before comparing

--- tester/tester.sh
+++ tester/tester.sh
@@ -420,10 +420,15 @@ for((i=1;i<=TST;i++)); do
 		fi
 	else
 		cp $PROBLEMPATH/out/output$i.txt correctout
-		if [ "$DIFFOPTION" = "ignore" ];then #removing all newlines and whitespaces before diff
+		if [ "$DIFFOPTION" = "ignore" ];then
+			# Removing all newlines and whitespaces before diff
 			tr -d ' \t\n\r\f' <out >tmp1 && mv tmp1 out;
 			tr -d ' \t\n\r\f' <correctout >tmp1 && mv tmp1 correctout;
 		fi
+		# Add a newline at the end of both files
+		echo "" >> out
+		echo "" >> correctout
+		# Compare output files
 		if $DIFFTOOL $DIFFARGUMENT out correctout >/dev/null 2>/dev/null
 		then
 			ACCEPTED=true
